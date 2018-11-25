c390bb7b461e42b0d32b48278a14183fcc4b38b5
Trevor Stinson <trevor@trevorstinson.com>
2018-11-25 14:04:18 -0600

    Routes

--- src/main/java/com/trevorstinson/hellospring/controllers/HelloController.java
+++ src/main/java/com/trevorstinson/hellospring/controllers/HelloController.java
@@ -1,23 +1,56 @@
 package com.trevorstinson.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-import javax.xml.ws.RequestWrapper;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
+//@RequestMapping("hello")
 public class HelloController {
 
     @RequestMapping(value = "")
     @ResponseBody
-    public String index() {
-        return "Hello World";
+    public String index(HttpServletRequest request) {
+
+        String name = request.getParameter("name");
+
+        if (name == null) {
+            name = "World";
+        }
+
+        return "Hello " + name;
+    }
+
+    @RequestMapping(value = "hello", method = RequestMethod.GET)
+    @ResponseBody
+    public String helloForm() {
+
+        String html = "<form method='post'>" +
+                "<input type='text' name='name' />" +
+                "<input type='submit' value='Greet Me!' />" +
+                "</form>";
+
+        return html;
+    }
+
+    @RequestMapping(value = "hello", method = RequestMethod.POST)
+    @ResponseBody
+    public String helloPost(HttpServletRequest request) {
+
+        String name = request.getParameter("name");
+
+        return "Hello " + name;
+    }
+
+    @RequestMapping(value = "hello/{name}")
+    @ResponseBody
+    public String helloUrlSegment(@PathVariable String name) {
+        return "Hello " + name;
     }
 
     @RequestMapping(value = "goodbye")
-    @ResponseBody
     public String goodbye() {
-        return "Goodbye";
+        return "redirect:/";
     }
 }
